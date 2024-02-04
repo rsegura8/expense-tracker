b187a91569fda1cfb66faf1fc7ee13d651b0b0ae
rsegura8 <areyousure.rey@gmail.com>
2024-02-03 22:46:59 -0600

    Added Balance

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <script setup>
   import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+  import { ref,computed } from 'vue';
+
+  const transactions = ref([
+    {id: 1, text: 'Paycheck', amount: 699.99},
+    {id: 2, text: 'Food', amount: -20},
+    {id: 3, text: 'Bills', amount: -200},
+    {id: 4, text: 'Video Game', amount: -54.11},
+    {id: 5, text: 'Tax Return', amount: 3000},
+  ])
+
+  //get the total
+  const total = computed(() => {
+    return transactions.value.reduce( (acc, transaction) => {
+      return acc + transaction.amount
+    }, 0)
+  })
+
+  
 </script>
 
 <template>
   <Header></Header>
+  <div class="container">
+    <Balance :total="total"></Balance>
+  </div>
 </template>
